bee77b67c81cd76e1d79d16bc1f1cdcfda903b14
enagikh <enagikh@ozon.ru>
2024-04-23 22:31:21 +0300

    add

--- AppServer/src/main/java/server/appserver/Main.java
+++ AppServer/src/main/java/server/appserver/Main.java
@@ -3,10 +3,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppServerApplication {
-
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(AppServerApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
-
 }
